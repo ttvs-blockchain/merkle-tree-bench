2335d5553d3a5ffa518055f65a5cd5c96d1d58ec
Tommy TIAN <xtianae@connect.ust.hk>
2022-08-12 01:22:46 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/ttvs-blockchain/merkle-tree-bench
+
+go 1.19
+
+require github.com/tommytim0515/go-merkletree v0.1.4
+
+require golang.org/x/sync v0.0.0-20220722155255-886fb9371eb4 // indirect
---
+++ main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"crypto/rand"
+	"fmt"
+	mt "github.com/tommytim0515/go-merkletree"
+	"math"
+	"time"
+)
+
+const (
+	testSize = 1024
+	trial    = 8096
+)
+
+// Binding is the binding of personal information hash and certificate information hash
+type Binding struct {
+	PersonInfoHash []byte `json:"person_info_hash"`
+	CertInfoHash   []byte `json:"certificate"`
+}
+
+func (b *Binding) Serialize() ([]byte, error) {
+	return append(b.PersonInfoHash, b.CertInfoHash...), nil
+}
+
+func genTestBindings(size int) []mt.DataBlock {
+	var bindings []mt.DataBlock
+	for i := 0; i < size; i++ {
+		bindings = append(bindings, &Binding{
+			PersonInfoHash: make([]byte, 32),
+			CertInfoHash:   make([]byte, 32),
+		})
+		_, err := rand.Read(bindings[i].(*Binding).PersonInfoHash)
+		handleError(err)
+		_, err = rand.Read(bindings[i].(*Binding).CertInfoHash)
+		handleError(err)
+	}
+	return bindings
+}
+
+func main() {
+	config := &mt.Config{
+		AllowDuplicates: true,
+	}
+	tree := mt.NewMerkleTree(config)
+	bindings := genTestBindings(testSize)
+	var totalTime float64
+	times := make([]float64, trial)
+	for i := 0; i < trial; i++ {
+		startTime := time.Now()
+		err := tree.Build(bindings)
+		handleError(err)
+		timeInterval := time.Since(startTime).Seconds()
+		totalTime += timeInterval
+		times[i] = timeInterval
+	}
+	avgTime := totalTime / float64(trial)
+	// standard deviation
+	var sum float64
+	for i := 0; i < trial; i++ {
+		sum += (times[i] - avgTime) * (times[i] - avgTime)
+	}
+	std := stdDev(times, avgTime)
+	fmt.Println("avg:", avgTime)
+	fmt.Println("std:", std)
+}
+
+func stdDev(numbers []float64, mean float64) float64 {
+	total := 0.0
+	for _, number := range numbers {
+		total += math.Pow(number-mean, 2)
+	}
+	variance := total / float64(len(numbers)-1)
+	return math.Sqrt(variance)
+}
+
+func handleError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
